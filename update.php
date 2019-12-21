533a3b6d03c451e8ee30b70970d2249cfaef1ad4
AlienCube1 <marcelfrancuz@gmail.com>
2019-12-21 09:26:02 +0100

    Removed login from update.php to separete 'logout.php' file to remove confusion

--- update.php
+++ update.php
@@ -1,15 +1,6 @@
 <?php
 
-  session_start();
-  unset($_SESSION["loggedin"]);
-  unset($_SESSION["username"]);
-  unset($_SESSION['wrong_username_or_pw']);
-  session_destroy();
-  setcookie('username', "", time() - 3600);
-  setcookie('password', "", time() - 3600);
-  header("Location: index.php");
 
-  //die;
 
 
  ?>
